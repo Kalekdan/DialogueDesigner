50fa1076373db018bfa5ca80148545e6f236e1de
Joe Rickard <joerickard@hotmail.co.uk>
2021-05-10 11:47:00 +0100

    Added JavaDoc to the DialogueEntry class and removed unused getters

--- src/main/java/com/pixolestudios/DialogueEntry.java
+++ src/main/java/com/pixolestudios/DialogueEntry.java
@@ -3,9 +3,11 @@ package main.java.com.pixolestudios;
 import com.sun.istack.internal.Nullable;
 
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * Each entry in a dialogue must be created as an instance of this linked class
+ */
 public class DialogueEntry {
     public static int DIdCount = 0;
 
@@ -15,6 +17,17 @@ public class DialogueEntry {
     private String DContent;
     private boolean isOption = true;
 
+    /**
+     * Default constructor for a new dialogue entry
+     * @param parent the dialogue entry which this follows on from. In the case this is the first entry in a dialogue
+     *               this should be set to -1
+     * @param content the content of the dialogue, usually a string of text
+     * @param isOption the isOption flag signifies whether this piece of dialogue is one that requires an option from
+     *                 the user. Questions or dialogue from the NPC speaker should be flagged as false, whilst options
+     *                 in response from the player should be flagged as true e.g.
+     *                 "Hello Traveller, what is youre name" should be false
+     *                 "Steve", "Bob", "Janet" should all be true
+     */
     public DialogueEntry(@Nullable DialogueEntry parent, String content, boolean isOption) {
         DId = ++ DIdCount;
         DContent = content;
@@ -43,24 +56,12 @@ public class DialogueEntry {
         this(null, content, isOption);
     }
 
-    public int getId() {
-        return DId;
-    }
-
-    public int getParentId() {
-        return parentDId;
-    }
-
-    public ArrayList<DialogueEntry> getChildren() {
-        return children;
-    }
-
+    /**
+     * Adds the provided DialogueEntry as a child to the instance of the class calling the method
+     * @param child the DialogueEntry to be added to the child of the instance of the class calling the method
+     */
     private void addChild(DialogueEntry child){
         children.add(child);
-    }
-
-    public String getContent() {
-        return DContent;
     }
 
     @Override
@@ -68,6 +69,10 @@ public class DialogueEntry {
         return "Id:" + DId + "\nContent:" + DContent +"\nChildren:" + children.size();
     }
 
+    /**
+     * Returns the content of all the children of a given DialogueEntry
+     * @return the options as a string, with the position labelled
+     */
     public String getOptions(){
         String response = "";
         for (int counter = 0; counter < children.size(); counter++) {
@@ -76,7 +81,10 @@ public class DialogueEntry {
         return response;
     }
 
-
+    /**
+     * Main function to step through a dialogue tree, allowing the user to select responses
+     * @param startPoint The DialogueEntry where the dialogue should begin
+     */
     public static void stepThrough(DialogueEntry startPoint){
         if (startPoint.children.size() == 0) return;
         if (!startPoint.isOption){System.out.println(startPoint.DContent);};
@@ -90,6 +98,11 @@ public class DialogueEntry {
         }
     }
 
+    /**
+     * Takes the users input from the console to select a specific DialogueEntry from an ArrayList provided
+     * @param children the ArrayList<DialogueEntry> that the selection is to be made from
+     * @return the selected DialogueEntry
+     */
     public static DialogueEntry selectChild(ArrayList<DialogueEntry> children){
         Scanner sc = new Scanner(System.in);    //System.in is a standard input stream
         int selection= sc.nextInt();
